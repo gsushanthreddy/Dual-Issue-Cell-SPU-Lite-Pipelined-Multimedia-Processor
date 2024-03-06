37d868cf9b0984bedf03ecff372530f25995c988
Akarsh-Gokavarapu <akarsh.gokavarapu.1803@gmail.com>
2024-03-06 16:49:52 -0500

    changed little endian to big endian in register file and commented rc related inputs and outputs

--- CellSPU/registerFile.sv
+++ CellSPU/registerFile.sv
@@ -1,26 +1,26 @@
 module registerFile(
     input clock,
     input reset,
-    input logic [6:0] ra_ep_address,
-    input logic [6:0] rb_ep_address, 
-    input logic [6:0] rc_ep_address,
-    input logic [6:0] rt_ep_address,
-    input logic [127:0] rt_value_ep,
+    input logic [0:6] ra_ep_address,
+    input logic [0:6] rb_ep_address, 
+    input logic [0:6] rc_ep_address,
+    input logic [0:6] rt_ep_address,
+    input logic [0:127] rt_value_ep,
     input wrt_en_ep, 
-    input logic [6:0] ra_op_address, 
-    input logic [6:0] rb_op_address, 
-    input logic [6:0] rc_op_address,
-    input logic [6:0] rt_op_address,
-    input logic [127:0] rt_value_op,
+    input logic [0:6] ra_op_address, 
+    input logic [0:6] rb_op_address, 
+    //input logic [0:6] rc_op_address,
+    input logic [0:6] rt_op_address,
+    input logic [0:127] rt_value_op,
     input wrt_en_op,
-    output logic [127:0] ra_ep_value,
-    output logic [127:0] rb_ep_value,
-    output logic [127:0] rc_ep_value,
-    output logic [127:0] ra_op_value,
-    output logic [127:0] rb_op_value,
-    output logic [127:0] rc_op_value
+    output logic [0:127] ra_ep_value,
+    output logic [0:127] rb_ep_value,
+    output logic [0:127] rc_ep_value,
+    output logic [0:127] ra_op_value,
+    output logic [0:127] rb_op_value
+    //output logic [0:127] rc_op_value
 );
-    logic [127:0] reg_file[128];
+    logic [0:127] reg_file[128];
     always_comb
     begin
         ra_ep_value = reg_file[ra_ep_address];
@@ -28,7 +28,7 @@ module registerFile(
         rc_ep_value = reg_file[rc_ep_address];
         ra_op_value = reg_file[ra_op_address];
         rb_op_value = reg_file[rb_op_address];
-        rc_op_value = reg_file[rc_op_address];
+        //rc_op_value = reg_file[rc_op_address];
     end
 
     always_ff @(posedge clock) begin
@@ -41,7 +41,7 @@ module registerFile(
         begin
             reg_file[rt_op_address] <= rt_value_op;
         end
-        // What if wrt_en_ep and wrt_en_op are both 1 and they write to same RT address? Which value from either of the pipes should RT store?
+        // What if wrt_en_ep and wrt_en_op are both 1 and they write to same RT address? Which value from either of the pipes should RT store? Stall odd pipe 
     end
 
 endmodule
