b9b9eabf202f2fbba305c4cb7246d006689a48c2
Akarsh-Gokavarapu <akarsh.gokavarapu.1803@gmail.com>
2024-04-03 02:33:05 -0400

    updated decode till thw 9 bit opcode instructions of even pipe

--- CellSPU/decode_stage.sv
+++ CellSPU/decode_stage.sv
@@ -47,6 +47,16 @@ module decode_stage(
     logic [0:6] ep_rb_2_address;
     logic [0:6] ep_rc_2_address;
 
+    logic [0:17] ep_I18_1;
+    logic [0:15] ep_I16_1;
+    logic [0:9] ep_I10_1;
+    logic [0:6] ep_I7_1;
+    logic [0:17] ep_I18_2;
+    logic [0:15] ep_I16_2;
+    logic [0:9] ep_I10_2;
+    logic [0:6] ep_I7_2;
+    
+
     always_comb begin
         if(first_inst_4 == 4'b1110 || first_inst_4 == 4'b1101 || first_inst_4 == 4'b1111 || first_inst_4 == 4'b1100) begin
             ep_inst1_flag = 1;
@@ -98,6 +108,234 @@ module decode_stage(
                 4'b1100:
                     begin
                         ep_opcode_2 = MULTIPLY_AND_ADD;
+                    end
+            endcase
+        end
+
+        if(first_inst_7 == 7'b0100001 ) begin
+            ep_inst1_flag = 1;
+            op_inst1_flag = 0;
+            ep_I18_1 = first_inst[7:24]
+            ep_rt_1_address = first_inst[25:31];
+
+            ep_opcode_1 = IMMEDIATE_LOAD_ADDRESS;
+    
+        end
+
+        if(second_inst_7 == 7'b0100001 ) begin
+            ep_inst2_flag = 1;
+            op_inst2_flag = 0;
+            ep_I18_2 = second_inst[7:24]
+            ep_rt_2_address = second_inst[25:31];
+
+            ep_opcode_2 = IMMEDIATE_LOAD_ADDRESS;
+    
+        end
+
+        if(first_inst_8 == 8'b00011101 || first_inst_8 == 8'b00011100 || first_inst_8 == 8'b00001101 || first_inst_8 == 8'b00001100 || first_inst_8 == 8'b00010101 || first_inst_8 == 8'b00010100 || first_inst_8 == 8'b00000101 || first_inst_8 == 8'b00000100 || first_inst_8 == 8'b01000101 || first_inst_8 == 8'b01000100 || first_inst_8 == 8'b01111101 || first_inst_8 = 8'b01111100 || first_inst_8 == 8'b01001101 || first_inst_8 == 8'b01001100 || first_inst_8 == 8'b01011101 || first_inst_8 == 8'b01011100 || first_inst_8 == 8'b01110100 || first_inst_8 == 8'b01110101) begin
+            ep_inst1_flag = 1;
+            op_inst1_flag = 0;
+            ep_I10_1 = first_inst[8:17];
+            ep_ra_1_address = first_inst[18:24];
+            ep_rt_1_address = first_inst[25:31];
+            
+            case(first_inst_8)
+                8'b00011101:
+                    begin
+                        ep_opcode_1 = ADD_HALFWORD_IMMEDIATE;
+                    end
+                8'b00011100:
+                    begin
+                        ep_opcode_1 = ADD_WORD_IMMEDIATE;
+                    end
+                8'b00001101:
+                    begin
+                        ep_opcode_1 = SUBTRACT_FROM_HALFWORD_IMMEDIATE;
+                    end
+                8'b00001100:
+                    begin
+                        ep_opcode_1 = SUBTRACT_FROM_WORD_IMMEDIATE;
+                    end
+                8'b00010101:
+                    begin
+                        ep_opcode_1 = AND_HALFWORD_IMMEDIATE;
+                    end
+                8'b00010100:
+                    begin
+                        ep_opcode_1 = AND_WORD_IMMEDIATE;
+                    end
+                8'b00000101:
+                    begin
+                        ep_opcode_1 = OR_HALFWORD_IMMEDIATE;
+                    end
+                8'b00000100:
+                    begin
+                        ep_opcode_1 = OR_WORD_IMMEDIATE;
+                    end
+                8'b01000101:
+                    begin
+                        ep_opcode_1 = EXCLUSIVE_OR_HALFWORD_IMMEDIATE;
+                    end
+                8'b01000100:
+                    begin
+                        ep_opcode_1 = EXCLUSIVE_OR_WORD_IMMEDIATE;
+                    end
+                8'b01111101:
+                    begin
+                        ep_opcode_1 = COMPARE_EQUAL_HALFWORD_IMMEDIATE;
+                    end
+                8'b01111100:
+                    begin
+                        ep_opcode_1 = COMPARE_EQUAL_WORD_IMMEDIATE;
+                    end
+                8'b01001101:
+                    begin
+                        ep_opcode_1 = COMPARE_GREATER_THAN_HALFWORD_IMMEDIATE;
+                    end
+                8'b01001100:
+                    begin
+                        ep_opcode_1 = COMPARE_GREATER_THAN_WORD_IMMEDIATE;
+                    end
+                8'b01011101:
+                    begin
+                        ep_opcode_1 = COMPARE_LOGICAL_GREATER_THAN_HALFWORD_IMMEDIATE;
+                    end
+                8'b01011100:
+                    begin
+                        ep_opcode_1 = COMPARE_LOGICAL_GREATER_THAN_WORD_IMMEDIATE;
+                    end
+                8'b01110100:
+                    begin
+                        ep_opcode_1 = MULTIPLY_IMMEDIATE;
+                    end
+                8'b01110101:
+                    begin
+                        ep_opcode_1 = MULTIPLY_UNSIGNED_IMMEDIATE;
+                    end
+            endcase
+        end
+
+        if(second_inst_8 == 8'b00011101 || second_inst_8 == 8'b00011100 || second_inst_8 == 8'b00001101 || second_inst_8 == 8'b00001100 || second_inst_8 == 8'b00010101 || second_inst_8 == 8'b00010100 || second_inst_8 == 8'b00000101 || second_inst_8 == 8'b00000100 || second_inst_8 == 8'b01000101 || second_inst_8 == 8'b01000100 || second_inst_8 == 8'b01111101 || second_inst_8 = 8'b01111100 || second_inst_8 == 8'b01001101 || second_inst_8 == 8'b01001100 || second_inst_8 == 8'b01011101 || second_inst_8 == 8'b01011100 || second_inst_8 == 8'b01110100 || second_inst_8 == 8'b01110101) begin
+            ep_inst2_flag = 1;
+            op_inst2_flag = 0;
+            ep_I10_2 = second_inst[8:17];
+            ep_ra_2_address = second_inst[18:24];
+            ep_rt_2_address = second_inst[25:31];
+            
+            case(second_inst_8)
+                8'b00011101:
+                    begin
+                        ep_opcode_2 = ADD_HALFWORD_IMMEDIATE;
+                    end
+                8'b00011100:
+                    begin
+                        ep_opcode_2 = ADD_WORD_IMMEDIATE;
+                    end
+                8'b00001101:
+                    begin
+                        ep_opcode_2 = SUBTRACT_FROM_HALFWORD_IMMEDIATE;
+                    end
+                8'b00001100:
+                    begin
+                        ep_opcode_2 = SUBTRACT_FROM_WORD_IMMEDIATE;
+                    end
+                8'b00010101:
+                    begin
+                        ep_opcode_2 = AND_HALFWORD_IMMEDIATE;
+                    end
+                8'b00010100:
+                    begin
+                        ep_opcode_2 = AND_WORD_IMMEDIATE;
+                    end
+                8'b00000101:
+                    begin
+                        ep_opcode_2 = OR_HALFWORD_IMMEDIATE;
+                    end
+                8'b00000100:
+                    begin
+                        ep_opcode_2 = OR_WORD_IMMEDIATE;
+                    end
+                8'b01000101:
+                    begin
+                        ep_opcode_2 = EXCLUSIVE_OR_HALFWORD_IMMEDIATE;
+                    end
+                8'b01000100:
+                    begin
+                        ep_opcode_2 = EXCLUSIVE_OR_WORD_IMMEDIATE;
+                    end
+                8'b01111101:
+                    begin
+                        ep_opcode_2 = COMPARE_EQUAL_HALFWORD_IMMEDIATE;
+                    end
+                8'b01111100:
+                    begin
+                        ep_opcode_2 = COMPARE_EQUAL_WORD_IMMEDIATE;
+                    end
+                8'b01001101:
+                    begin
+                        ep_opcode_2 = COMPARE_GREATER_THAN_HALFWORD_IMMEDIATE;
+                    end
+                8'b01001100:
+                    begin
+                        ep_opcode_2 = COMPARE_GREATER_THAN_WORD_IMMEDIATE;
+                    end
+                8'b01011101:
+                    begin
+                        ep_opcode_2 = COMPARE_LOGICAL_GREATER_THAN_HALFWORD_IMMEDIATE;
+                    end
+                8'b01011100:
+                    begin
+                        ep_opcode_2 = COMPARE_LOGICAL_GREATER_THAN_WORD_IMMEDIATE;
+                    end
+                8'b01110100:
+                    begin
+                        ep_opcode_2 = MULTIPLY_IMMEDIATE;
+                    end
+                8'b01110101:
+                    begin
+                        ep_opcode_2 = MULTIPLY_UNSIGNED_IMMEDIATE;
+                    end
+            endcase
+        end
+
+        if(first_inst_9 == 9'b010000011 || first_inst_9 == 9'b010000010 || first_inst_9 == 9'b010000001) begin
+            ep_inst1_flag = 1;
+            op_inst1_flag = 0;
+            ep_I16_1 = first_inst[9:24];
+            ep_rt_1_address = first_inst[25:31];
+            case(first_inst_9)
+                9'b010000011:
+                    begin
+                        ep_opcode_1 = IMMEDIATE_LOAD_HALFWORD;
+                    end
+                9'b010000010:
+                    begin
+                        ep_opcode_1 = IMMEDIATE_LOAD_HALFWORD_UPPER;
+                    end
+                9'b010000001:
+                    begin
+                        ep_opcode_1 = IMMEDIATE_LOAD_WORD;
+                    end
+            endcase
+        end
+
+        if(second_inst_9 == 9'b010000011 || second_inst_9 == 9'b010000010 || second_inst_9 == 9'b010000001) begin
+            ep_inst2_flag = 1;
+            op_inst2_flag = 0;
+            ep_I16_2 = second_inst[9:24];
+            ep_rt_2_address = second_inst[25:31];
+            case(first_inst_9)
+                9'b010000011:
+                    begin
+                        ep_opcode_2 = IMMEDIATE_LOAD_HALFWORD;
+                    end
+                9'b010000010:
+                    begin
+                        ep_opcode_2 = IMMEDIATE_LOAD_HALFWORD_UPPER;
+                    end
+                9'b010000001:
+                    begin
+                        ep_opcode_2 = IMMEDIATE_LOAD_WORD;
                     end
             endcase
         end
