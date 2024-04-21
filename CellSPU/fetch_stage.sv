5653def54cc06ca774827e71d64582fd3fc7696f
Akarsh-Gokavarapu <akarsh.gokavarapu.1803@gmail.com>
2024-04-21 18:26:45 -0400

    made pc changes in fetch

--- CellSPU/fetch_stage.sv
+++ CellSPU/fetch_stage.sv
@@ -17,23 +17,27 @@ module fetch_stage(
     end
 
     always_ff @(posedge clock) begin
-        pc <= pc_input;
         if(stall==0) begin
             if(branch_taken==0) begin
+                pc <= pc_output;
                 first_inst <= {instruction_memory[pc],instruction_memory[pc+1],instruction_memory[pc+2],instruction_memory[pc+3]};
                 second_inst <= {instruction_memory[pc+4],instruction_memory[pc+5],instruction_memory[pc+6],instruction_memory[pc+7]};
+                pc<=pc+8;
             end
             else begin
+                pc <= pc_input;
                 if(pc_input[29]==1) begin
-                    first_inst <= {11'b00000000001,21'bx};
+                    first_inst <= {11'b00000000001,21'bx};/// this hazard should not reflect in the decode stage if the second instruction is odd
                     second_inst <= {instruction_memory[pc],instruction_memory[pc+1],instruction_memory[pc+2],instruction_memory[pc+3]};
+                    pc<=pc+4;
                 end
                 else begin
                     first_inst <= {instruction_memory[pc],instruction_memory[pc+1],instruction_memory[pc+2],instruction_memory[pc+3]};
                     second_inst <= {instruction_memory[pc+4],instruction_memory[pc+5],instruction_memory[pc+6],instruction_memory[pc+7]};
+                    pc<=pc+8;
                 end
             end
-            pc<=pc+8;
+            
         end
 
         else begin
